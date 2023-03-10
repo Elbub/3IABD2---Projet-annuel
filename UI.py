bd541521b493f00305b4a8c979b37edb23e4651d
Elbub <elbub.gamer@laposte.net>
2023-03-10 10:17:44 +0100

    First initialisation

---
+++ UI.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+### Opérations sur les fichiers
+import os
+from os import path
+from json import *
+from turtle import done
+import pandas
+
+### Interface graphique
+from tkinter import *
+from tkinter import ttk
+# from tkinter import tix # Obsolète, à remplacer
+from tkinter.messagebox import *
+from tkinter.filedialog import *
+from screeninfo import get_monitors
+
+### Divers
+from numpy import pi
+import time
+import datetime
+import re
+from threading import Event, Thread
+
+### Debug
+import sys
+import random

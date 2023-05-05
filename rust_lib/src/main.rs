4c8291fbae3bcc518349bf5a51eb756bd4acb9cd
Elbub <elbub.gamer@laposte.net>
2023-05-05 21:48:03 +0200

    feature (linearModel.rs): added training function

--- rust_lib/src/main.rs
+++ rust_lib/src/main.rs
@@ -3,11 +3,13 @@ use rust_lib::linearModel;
 fn main() {
     println!("Hello, world!");
     //crate::linearModel;
-    let i_hope_this_works = linearModel::linear_model::points_array(10);
+    let i_hope_this_works = linearModel::linear_model::points_array(1000);
     let test_points_label = linearModel::linear_model::points_label(&i_hope_this_works);
 
-    println!("{:?}",i_hope_this_works);
-    println!("{:?}",test_points_label);
+    // println!("{:?}",i_hope_this_works);
+    // println!("{:?}",test_points_label);
 
-    linearModel::linear_model::create_chart(&i_hope_this_works);
+    //linearModel::linear_model::create_chart(&i_hope_this_works);
+    let w:Vec<f32> = linearModel::linear_model::linear_model_training(&test_points_label, &i_hope_this_works);
+    println!("{:?}",w);
 }

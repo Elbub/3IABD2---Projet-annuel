dfd00f23a77d7f834946d481bb13feb1df481466
clementdevarieux <devarieux.clement@gmail.com>
2023-05-06 18:57:25 +0200

    interops: premiers pas

--- rust_lib/src/linearModel.rs
+++ rust_lib/src/linearModel.rs
@@ -1,26 +1,18 @@
-pub mod linear_model{
+mod linear_model{
 
-    pub fn points_array(number_of_points: usize) -> Vec<Vec<f32>>{
+    #[no_mangle]
+    extern "C" fn points_array(number_of_points: usize) -> Vec<Vec<f32>>{
         use rand::Rng;
         let mut vec_of_points = Vec::with_capacity(number_of_points);
         for _ in 0..number_of_points {
-            // That...
-            // let x: f32 = rand::thread_rng().gen_range(0f32..10f32);
-            // let y: f32 = rand::thread_rng().gen_range(0f32..10f32);
-            // let mut coordinates : Vec<f32> = Vec::new();
-            // coordinates.push(x);
-            // coordinates.push(y);
-            // ... is equivalent to that.
             let coordinates : Vec<f32> = Vec::from([rand::thread_rng().gen_range(0f32..10f32), rand::thread_rng().gen_range(0f32..10f32)]);
             vec_of_points.push(coordinates);
-
-            // let tuple_of_points = (x,y);
-            // vec_of_points.push(tuple_of_points);
         }
         vec_of_points
     }
 
-    pub fn points_label(vec_of_points: &Vec<Vec<f32>>) -> Vec<f32>{
+    #[no_mangle]
+    extern "C" fn points_label(vec_of_points: &Vec<Vec<f32>>) -> Vec<f32>{
         let mut vec_of_labels:Vec<f32> = Vec::with_capacity(vec_of_points.len());
         // for i in 0..vec_of_points.len() {
         //     if -vec_of_points[i][1] + vec_of_points[i][0] + 0.25 >= 0. {     //-y + x + 0.25 = 0
@@ -35,35 +27,8 @@ pub mod linear_model{
         vec_of_labels
     }
 
-    pub fn create_chart(vec_of_points: &Vec<(f32, f32)>){
-        use plotters::prelude::*;
-        use plotters::coord::types::RangedCoordf32;
-
-        let root = BitMapBackend::new("/Users/bouai/Downloads/test.png", (600,400)).into_drawing_area();
-        root.fill(&RGBColor(240, 200, 200));
-        let root = root.apply_coord_spec(Cartesian2d::<RangedCoordf32, RangedCoordf32>::new(
-            0f32..1f32,
-            0f32..1f32,
-            (0..640, 0..480),
-        ));
-
-        let dot_and_label = |x: f32, y: f32| {
-            return EmptyElement::at((x, y))
-                + Circle::new((0, 0), 3, ShapeStyle::from(&BLACK).filled())
-                + Text::new(
-                format!("({:.2},{:.2})", x, y),
-                (10, 0),
-                ("sans-serif", 15.0).into_font(),
-            );
-        };
-
-        for i in 0..vec_of_points.len() {
-            root.draw(&dot_and_label(vec_of_points[i].0, vec_of_points[i].1));
-        }
-        root.present();
-    }
-
-    pub fn linear_model_training(labels : &Vec<f32>, points: &Vec<Vec<f32>>) -> Vec<f32> {wwwwwwwwwwww
+    #[no_mangle]
+    extern "C" fn linear_model_training(labels : &Vec<f32>, points: &Vec<Vec<f32>>) -> Vec<f32> {
         use rand::Rng;
         let mut rng = rand::thread_rng();
         let number_of_parameters:usize = points[0].len();
@@ -102,7 +67,8 @@ pub mod linear_model{
         w
     }
 
-    pub fn trained_model(number_of_points: usize) -> Vec<f32>{
+    #[no_mangle]
+    extern "C" fn trained_model(number_of_points: usize) -> Vec<f32>{
         let points = points_array(number_of_points);
         let label_points = points_label(&points);
 
--- rust_lib/src/main.rs
+++ rust_lib/src/main.rs
@@ -1,20 +1,7 @@
 use rust_lib::linearModel;
 
-fn main() {
-    // println!("Hello, world!");
-    // //crate::linearModel;
-    // let i_hope_this_works = linearModel::linear_model::points_array(1000);
-    // let test_points_label = linearModel::linear_model::points_label(&i_hope_this_works);
-    //
-    // // println!("{:?}",i_hope_this_works);
-    // // println!("{:?}",test_points_label);
-    //
-    // //linearModel::linear_model::create_chart(&i_hope_this_works);
-    // let w:Vec<f32> = linearModel::linear_model::linear_model_training(&test_points_label, &i_hope_this_works);
-    //
-    // println!("{:?}",w);
-
+#[no_mangle]
+extern "C" fn main() {
     let trained_weights = linearModel::linear_model::trained_model(100);
-
     println!("{:?}",trained_weights);
 }

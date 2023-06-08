7330c65a2c0b6fb2745f7370d4d0815353cb0deb
clementdevarieux <devarieux.clement@gmail.com>
2023-06-08 21:41:50 +0200

    essais infructueux de modele lineaire regression

--- rust_lib/src/lib.rs
+++ rust_lib/src/lib.rs
@@ -1,5 +1,7 @@
 //mod multyLayerPerceptron;
 
+use nalgebra::DMatrix;
+
 #[no_mangle]
 pub extern "C" fn points_array(number_of_points: usize, dimension: usize) -> *mut f32{
     use rand::Rng;
@@ -81,20 +83,15 @@ extern "C" fn linear_model_training(w_ptr: *mut f32, labels_ptr : *mut f32, vec_
     unsafe {
         use rand::Rng;
         let vec_of_points = std::slice::from_raw_parts(vec_of_points_ptr,
-                                                arr_size*arr_dimension);
+                                                       arr_size * arr_dimension);
         let labels = std::slice::from_raw_parts(labels_ptr,
                                                 arr_size);
-
         let mut w = Vec::from_raw_parts(
             w_ptr, arr_dimension + 1, arr_dimension + 1);
-
         let mut rng = rand::thread_rng();
-
         for _ in 0..epoch {
             let k: usize = rng.gen_range(0..arr_size);
-
             let y_k: f32 = labels[k];
-
             let mut x_k: Vec<f32> = Vec::with_capacity(arr_dimension + 1);
             x_k.push(1f32);
             for i in 0..arr_dimension {
@@ -112,7 +109,6 @@ extern "C" fn linear_model_training(w_ptr: *mut f32, labels_ptr : *mut f32, vec_
                 w[i] += learning_rate * (y_k - g_x_k) * x_k[i];
             }
         }
-
         // else {
         //     for _ in 0..epoch {
         //         let k: usize = rng.gen_range(0..(arr_size/number_of_classes)) * number_of_classes;
@@ -140,10 +136,60 @@ extern "C" fn linear_model_training(w_ptr: *mut f32, labels_ptr : *mut f32, vec_
         //         }
         //     }
         // }
+
         let arr_slice = w.leak();
         arr_slice.as_mut_ptr()
     }
 }
+
+#[no_mangle]
+extern "C" fn find_w_lineard_regression(x_ptr: *mut f32, y_ptr: *mut f32, nombre_lignes_x_et_y: usize, nombre_colonnes_x: usize, nombre_colonnes_y: usize) -> *mut f32 {
+    unsafe {
+        // let mut w = Vec::with_capacity(nombre_lignes_x_et_y);
+        use nalgebra::*;
+
+        let x_vect = std::slice::from_raw_parts(x_ptr, nombre_lignes_x_et_y * nombre_colonnes_x);
+
+        let y_vect = std::slice::from_raw_parts(y_ptr, nombre_lignes_x_et_y * nombre_colonnes_y);
+
+        let mut x_mat = DMatrix::zeros(nombre_lignes_x_et_y, nombre_colonnes_x);
+        let mut y_mat = DMatrix::zeros(nombre_lignes_x_et_y, nombre_colonnes_y);
+
+        for i in 0..nombre_lignes_x_et_y {
+            for j in 0..nombre_colonnes_x {
+                x_mat[(i, j)] = x_vect[i * nombre_colonnes_x + j] as f32;
+            }
+        }
+
+        for i in 0..nombre_lignes_x_et_y {
+            for j in 0..nombre_colonnes_y {
+                y_mat[(i, j)] = y_vect[i * nombre_colonnes_y + j] as f32;
+            }
+        }
+
+
+        let x_transpose = x_mat.transpose();
+
+        let x_t_mult_x = x_transpose * x_mat.clone();
+
+        let inv_x_t_x = x_t_mult_x.try_inverse();
+
+        let new_x_trans = x_mat.transpose();
+
+        let inv_times_x_t = match inv_x_t_x {
+            Some(inv) => inv * new_x_trans,
+            None => panic!("Non inversible"),
+        };
+
+        let mut result_matrix = inv_times_x_t * y_mat;
+
+        let w: Vec<_> = result_matrix.iter().cloned().collect();
+
+        let arr_slice = w.leak();
+        arr_slice.as_mut_ptr()
+    }
+}
+
 
 #[no_mangle]
 extern "C" fn predict_linear_model(vec_to_predict_ptr: *const f32, trained_model_ptr: *mut f32, arr_size: usize, arr_dimension: usize) -> *mut f32{
